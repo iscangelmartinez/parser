8910df44aa4f20479fa85b2eee3b76c28e746ad8
iscangelmartinez <angeljesusmartinezfrias@hotmail.com>
2014-06-07 19:20:55 -0500

    primera modificacion

--- Parser/src/parser/Parser.java
+++ Parser/src/parser/Parser.java
@@ -17,6 +17,7 @@ public class Parser {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print(("Hola"));
         
     }
     
